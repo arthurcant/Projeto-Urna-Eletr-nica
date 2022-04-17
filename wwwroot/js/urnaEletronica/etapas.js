6fd524ce556d761f3e60503b737695498cdec43e
arthurcant <arthurbig19@gmail.com>
2022-04-17 20:09:42 -0300

    The combination of the two projects is  progress.

---
+++ wwwroot/js/urnaEletronica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'../../../wwwroot/images/candidatos/38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'../../../wwwroot/images/candidatos/77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'../../../wwwroot/images/candidatos/99.jpg', legenda: 'Prefeito'},
+                    {url:'../../../wwwroot/images/candidatos/99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'../../../wwwroot/images/candidatos/84.jpg', legenda: 'Prefeito'},
+                    {url:'../../../wwwroot/images/candidatos/84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
