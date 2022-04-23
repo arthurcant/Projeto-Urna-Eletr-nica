d17cb9558d07ed7b19379aa8b42420755436cb08
Paloma Alves <palomaalves@hotmail.com>
2022-04-23 18:00:14 -0300

    adiciona try e catch à requisição de conexão com o mongo

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
 async function main(){
-    await mongoose.connect('mongodb+srv://admin:adminUrnaEle@urnaeletronica.k9yl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-    console.log('Conect to Mongoose')
+    try {
+        await mongoose.connect('mongodb+srv://admin:adminUrnaEle@urnaeletronica.k9yl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        console.log('Conect to Mongoose')
+    } catch(error){
+        console.log('connection error')
+    }
 }
 
 main().catch((err) => console.log(`Erro in mongodb: ${err}`))
